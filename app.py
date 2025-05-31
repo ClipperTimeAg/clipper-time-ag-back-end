7e6e0392674fbbd034e7c77e62e15238d83f7585
Anderson Ramos <125606509+anderson-rdev@users.noreply.github.com>
2025-05-31 02:23:02 -0300

    Refatorando:  app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-from app.functions import senha_criptografada, armazenar_senha
+from app.functions import senha_criptografada, armazenar_senha, verificar_senha
 
 app = Flask(__name__)
 CORS(app)  # Habilita CORS para todas as rotas
@@ -20,7 +20,6 @@ def cadastrar():
     telefone = dados.get('telefone')
     senha = dados.get('senha')
 
-    # Validação simples
     if not all([nome, email, telefone, senha]):
         return jsonify(erro="Todos os campos são obrigatórios."), 400
 
@@ -29,6 +28,25 @@ def cadastrar():
         return jsonify(msg="Usuário cadastrado com sucesso!"), 201
     except Exception as e:
         return jsonify(erro=str(e)), 500
+
+@app.route('/login', methods=['POST'])
+def login():
+    dados = request.get_json()
+    print("Dados recebidos:", dados)  # Log dos dados recebidos
+
+    nome = dados.get('nome')  
+    senha = dados.get('senha')
+
+    if not all([nome, senha]):
+        return jsonify(erro="Nome ou senha incorretos"), 400
+
+    resultado = verificar_senha(nome, senha)
+    print("Resultado da verificação:", resultado)  # Log do resultado da verificação
+
+    if resultado['sucesso'] == "True":
+        return jsonify(msg=resultado['mensagem']), 200
+    else:
+        return jsonify(erro=resultado['mensagem']), 401
 
 if __name__ == '__main__':
     app.run(debug=True)
